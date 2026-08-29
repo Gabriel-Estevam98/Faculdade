180f4ae96e5e2343279bbf73338df7f5cb01b25c
Gabriel-Estevam98 <gbestevam0@hotmail.com>
2026-08-28 22:18:45 -0300

    att. calculadora de datas

---
+++ Calculadora de Datas.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+data1 = input("digite a primeira data:")
+data2 = input("digite a segunda data:")
+
+
+
+data1 = datetime.strptime(data1, "%d/%m/%Y")
+data2 = datetime.strptime(data2, "%d/%m/%Y")
+
+resultado = data2 - data1
+
+print(resultado.days)
